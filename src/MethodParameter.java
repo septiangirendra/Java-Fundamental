327c4aaad86565cd639e59ba1c98653a7a82989a
septiangirendra <septiangirendra@gmail.com>
2022-04-12 16:01:03 +0700

    Method, Method Parameter

---
+++ src/MethodParameter.java
@@ -0,0 +1,12 @@
+public class MethodParameter {
+
+    public static void main(String[] args) {
+
+        sayHello("Irmandaru","Raiza");
+
+    }
+
+    static  void sayHello(String firstName, String lastName){
+        System.out.println("My Name" + firstName +" "+ lastName);
+    }
+}
